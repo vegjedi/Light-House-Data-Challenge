b576cf0b573b021bdd561cab420fda472b371454
Plant Power <69030924+vegjedi@users.noreply.github.com>
2022-04-13 23:03:36 -0700

    Add files via upload

---
+++ day04.py
@@ -0,0 +1,11 @@
+starters = {
+    "Potato Pancakes": 7.99,
+    "Salami Platter": 10.29,
+    "Brezel": 6.99,
+    "Maultaschen": 9.99,
+    "Fried Potatoes": 4.99
+}
+
+x=('Brezel', 6.99)
+
+print(x[0])
